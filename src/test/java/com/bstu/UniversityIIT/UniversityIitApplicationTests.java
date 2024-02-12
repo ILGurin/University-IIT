33be2aa57007caad2be6968cef81c20af35a89bf
ILGurin <126956586+ILGurin@users.noreply.github.com>
2024-02-12 22:14:25 +0300

    Initial commit

---
+++ src/test/java/com/bstu/UniversityIIT/UniversityIitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bstu.UniversityIIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityIitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
